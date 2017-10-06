01d11ea1831482ec30a82ccf001e1f072dea9597
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-10-06 23:54:02 +0300

    tests

--- client_test.go
+++ client_test.go
@@ -36,7 +36,7 @@ func TestClient_Count(t *testing.T) {
 	client.Count("a.b.c", 320, 0.9999)
 
 	if client.keyBuffer["a.b.c"] != "320|c|@0.9999" {
-		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
+		t.Errorf("Wrong count metric with rate: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
 
@@ -45,7 +45,25 @@ func TestClient_Count_NoRate(t *testing.T) {
 	client.Count("a.b.c", 320, 1)
 
 	if client.keyBuffer["a.b.c"] != "320|c" {
-		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
+		t.Errorf("Wrong count metric without rate: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_Gauge(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.Gauge("a.b.c", 320)
+
+	if client.keyBuffer["a.b.c"] != "320|g" {
+		t.Errorf("Wrong gauge metric: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_Set(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.Set("a.b.c", 320)
+
+	if client.keyBuffer["a.b.c"] != "320|s" {
+		t.Errorf("Wrong set metric: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
 
