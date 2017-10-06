f203d6225f9de09fba50f1a8178c137656c5aec4
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-10-06 23:35:33 +0300

    tests

--- client_test.go
+++ client_test.go
@@ -15,18 +15,37 @@ func TestClient_NewClient(t *testing.T) {
 
 func TestClient_Timing(t *testing.T) {
 	client := NewClient("127.0.0.1", 9876)
-	client.Timing("a.b.c", 320, 0.954)
+	client.Timing("a.b.c", 320, 0.9999)
 
-	if client.keyBuffer["a.b.c"] != "320|ms|@0.954" {
+	if client.keyBuffer["a.b.c"] != "320|ms|@0.9999" {
+		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_Timing_NoRate(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.Timing("a.b.c", 320, 1)
+
+	if client.keyBuffer["a.b.c"] != "320|ms" {
 		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
 
 func TestClient_Count(t *testing.T) {
 	client := NewClient("127.0.0.1", 9876)
-	client.Count("a.b.c", 320, 0.954)
+	client.Count("a.b.c", 320, 0.9999)
 
-	if client.keyBuffer["a.b.c"] != "320|c|@0.954" {
+	if client.keyBuffer["a.b.c"] != "320|c|@0.9999" {
 		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
+
+func TestClient_Count_NoRate(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.Count("a.b.c", 320, 1)
+
+	if client.keyBuffer["a.b.c"] != "320|c" {
+		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
