d1997fcf797bd3076a143acc1f4b54f8f3f75276
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-10-07 00:35:56 +0300

    test

--- client_test.go
+++ client_test.go
@@ -13,12 +13,35 @@ func TestClient_NewClient(t *testing.T) {
 	}
 }
 
-func TestClient_Timing(t *testing.T) {
+func TestClient_SetAutoflush(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+
+	client.SetAutoflush(true)
+	if client.autoflush != true {
+		t.Errorf("Autoflush must be true")
+	}
+
+	client.SetAutoflush(false)
+	if client.autoflush != false {
+		t.Errorf("Autoflush must be false")
+	}
+}
+
+func TestClient_Timing_BigRate(t *testing.T) {
 	client := NewClient("127.0.0.1", 9876)
 	client.Timing("a.b.c", 320, 0.9999)
 
-	if client.keyBuffer["a.b.c"] != "320|ms|@0.9999" {
-		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
+	if len(client.keyBuffer) > 0 && client.keyBuffer["a.b.c"] != "320|ms|@0.9999" {
+		t.Errorf("Wrong timing metric with big rate: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_Timing_SmallRate(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.Timing("a.b.c", 320, 0.0001)
+
+	if len(client.keyBuffer) > 0 && client.keyBuffer["a.b.c"] != "320|ms|@0.0001" {
+		t.Errorf("Wrong timing metric with small rate: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
 
@@ -27,16 +50,25 @@ func TestClient_Timing_NoRate(t *testing.T) {
 	client.Timing("a.b.c", 320, 1)
 
 	if client.keyBuffer["a.b.c"] != "320|ms" {
-		t.Errorf("Wrong timing metric: \"%s\"", client.keyBuffer["a.b.c"])
+		t.Errorf("Wrong timing metric without rate: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
 
-func TestClient_Count(t *testing.T) {
+func TestClient_Count_BigRate(t *testing.T) {
 	client := NewClient("127.0.0.1", 9876)
 	client.Count("a.b.c", 320, 0.9999)
 
-	if client.keyBuffer["a.b.c"] != "320|c|@0.9999" {
-		t.Errorf("Wrong count metric with rate: \"%s\"", client.keyBuffer["a.b.c"])
+	if len(client.keyBuffer) > 0 && client.keyBuffer["a.b.c"] != "320|c|@0.9999" {
+		t.Errorf("Wrong count metric with big rate: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_Count_SmallRate(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.Count("a.b.c", 320, 0.0001)
+
+	if len(client.keyBuffer) > 0 && client.keyBuffer["a.b.c"] != "320|c|@0.0001" {
+		t.Errorf("Wrong count metric with small rate: \"%s\"", client.keyBuffer["a.b.c"])
 	}
 }
 
@@ -64,6 +96,44 @@ func TestClient_Set(t *testing.T) {
 
 	if client.keyBuffer["a.b.c"] != "320|s" {
 		t.Errorf("Wrong set metric: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_addToBuffer(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+	client.addToBuffer("a.b.c", "320|s")
+	client.addToBuffer("a.b.d", "321|ms|@0.0001")
+
+	if len(client.keyBuffer) != 2 {
+		t.Errorf("Must be 2 keys in buffer")
+	}
+
+	if client.keyBuffer["a.b.c"] != "320|s" {
+		t.Errorf("Wrong metric added to buffer: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+
+	if client.keyBuffer["a.b.d"] != "321|ms|@0.0001" {
+		t.Errorf("Wrong metric added to buffer: \"%s\"", client.keyBuffer["a.b.c"])
+	}
+}
+
+func TestClient_isSendAcceptedBySampleRate(t *testing.T) {
+	client := NewClient("127.0.0.1", 9876)
+
+	if client.isSendAcceptedBySampleRate(2) == false {
+		t.Errorf("2 must be accepred by sample rate")
+	}
+
+	if client.isSendAcceptedBySampleRate(1) == false {
+		t.Errorf("1 must be accepred by sample rate")
+	}
+
+	if client.isSendAcceptedBySampleRate(0.00000001) == true {
+		t.Errorf("0.00000001 must not be accepred by sample rate")
+	}
+
+	if client.isSendAcceptedBySampleRate(0.99999999) == false {
+		t.Errorf("0.99999999 must not be accepred by sample rate")
 	}
 }
 
